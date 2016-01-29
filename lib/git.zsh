9d47c10fce9cb38336bf0d6a97c9660a9fa3e9c8
oneyoung <guowangyang@gmail.com>
2016-01-29 21:38:03 +0800

    * ignore the homerc git

--- lib/git.zsh
+++ lib/git.zsh
@@ -1,6 +1,10 @@
 # Outputs current branch info in prompt format
 function git_prompt_info() {
   local ref
+  # ignore the homerc git
+  if [[ $HOME ==  $(git rev-parse --show-toplevel) ]]; then
+     return
+  fi
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
